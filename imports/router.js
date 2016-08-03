3656744d00228a1c9a67e2e49e008fa08df2a693
huaiyudavid <davidyangrocs@gmail.com>
2016-08-02 23:08:24 -0500

    testing

--- imports/router.js
+++ imports/router.js
@@ -8,26 +8,32 @@ Router.route('/', {
     name: 'home',
     template: 'outline',
     onBeforeAction: function() {
+        var currentUser = Meteor.userId();
+        if(currentUser) {
+            console.log(currentUser);
+            console.log(Meteor.users.findOne(currentUser).location);
+            Router.go(Meteor.users.findOne(currentUser).location);
+            this.next();
+        } else {
+            this.render("login");
+        }
+
         // var currentUser = Meteor.userId();
-        // if(currentUser) {
-        //     console.log(currentUser);
-        //     console.log(Meteor.users.findOne(currentUser).location);
-        //     Router.go(Meteor.users.findOne(currentUser).location);
-        //     this.next();
+        // if (currentUser) {
+        //     var router = this;
+        //     Meteor.call('getUserLocation', function(error, result) {
+        //         console.log('result: ' + result);
+        //         Router.go(result);
+        //         router.next();
+        //     })
         // } else {
         //     this.render("login");
         // }
-
+    },
+    waitOn: function() {
         var currentUser = Meteor.userId();
         if (currentUser) {
-            var router = this;
-            Meteor.call('getUserLocation', function(error, result) {
-                console.log('result: ' + result);
-                Router.go(result);
-                router.next();
-            })
-        } else {
-            this.render("login");
+            return Meteor.subscribe('userData');
         }
     },
 });
