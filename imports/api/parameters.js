651814d04e2e941407ceed0a92d22fbbb4aa91a2
huaiyudavid <davidyangrocs@gmail.com>
2016-08-10 14:15:32 -0400

    switch to test mode

--- imports/api/parameters.js
+++ imports/api/parameters.js
@@ -43,22 +43,22 @@ export var Parameters = {
     testMode: false,        //whether or not to skip description
 
     readTreatments: function() {
-        practiceAdjacencyMatrices = readAdjMatrix("treatments/test_data/AMP_test.txt");
-        practiceParameterValues = readNetConfig("treatments/test_data/NCP_test.txt");
-        practiceBatchConfigs = readBatchConfig("treatments/test_data/BCP_test.txt");
+        practiceAdjacencyMatrices = readAdjMatrix("treatments/test_size_3 (adversarial)/AMP_test.txt");
+        practiceParameterValues = readNetConfig("treatments/test_size_3 (adversarial)/NCP_test.txt");
+        practiceBatchConfigs = readBatchConfig("treatments/test_size_3 (adversarial)/BCP_test.txt");
         
-        adjacencyMatrices = readAdjMatrix("treatments/test_data/AM_test.txt");
-        parameterValues = readNetConfig("treatments/test_data/NC_test.txt");
-        batchConfigs = readBatchConfig("treatments/test_data/BC_test.txt");
+        adjacencyMatrices = readAdjMatrix("treatments/test_size_3 (adversarial)/AM_test.txt");
+        parameterValues = readNetConfig("treatments/test_size_3 (adversarial)/NC_test.txt");
+        batchConfigs = readBatchConfig("treatments/test_size_3 (adversarial)/BC_test.txt");
 
-        // Full Experiment Data
-        practiceAdjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix_practice.txt");
-        practiceParameterValues = readNetConfig("treatments/input_data/network_configuration_practice.txt");
-        practiceBatchConfigs = readBatchConfig("treatments/input_data/batch_configuration_practice.txt");
+        // // Full Experiment Data
+        // practiceAdjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix_practice.txt");
+        // practiceParameterValues = readNetConfig("treatments/input_data/network_configuration_practice.txt");
+        // practiceBatchConfigs = readBatchConfig("treatments/input_data/batch_configuration_practice.txt");
         
-        adjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix.txt");
-        parameterValues = readNetConfig("treatments/input_data/network_configuration.txt");
-        batchConfigs = readBatchConfig("treatments/input_data/batch_configuration.txt");
+        // adjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix.txt");
+        // parameterValues = readNetConfig("treatments/input_data/network_configuration.txt");
+        // batchConfigs = readBatchConfig("treatments/input_data/batch_configuration.txt");
 
         // Determine the number of practice and proper games from the size of the arrays loaded above
         this.practiceGames = Math.min(practiceAdjacencyMatrices.length, practiceParameterValues.length);
--- server/server_startup.js
+++ server/server_startup.js
@@ -68,10 +68,10 @@ Meteor.startup(function () {
  
     // For testing purposes
     //// Mini test
-    /*
+    
     var users = ["1", "2", "3"];
     var password = "p";
-    */
+    
     
     //// Medium-scale test
     
@@ -79,10 +79,10 @@ Meteor.startup(function () {
     // var password = "p";
     
     
-    // Full-scale test
+    // // Full-scale test
     
-    var users = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25', '26', '27', '28', '29', '30'];
-    var password = "p";
+    // var users = ['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25', '26', '27', '28', '29', '30'];
+    // var password = "p";
     
     
     //// Full experiment [07.07.2016]
