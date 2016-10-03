fc4eb5a02f5e21b29bb7d2475f60ce0b2a9cfb46
huaiyudavid <davidyangrocs@gmail.com>
2016-10-03 00:11:18 -0500

    typo

--- imports/api/reputations.js
+++ imports/api/reputations.js
@@ -11,26 +11,28 @@ export var Reputations = {
         for (var i = 0; i < Participants.participants.length; i++) {
             var userId = Participants.participants[i];
             var name = Participants.id_name[userId];
-            reputationChoices[name] = [];
-            reputations[name] = 0;
+            this.reputationChoices[name] = [];
+            this.reputations[name] = 0;
 
             for (var j = 0; j < participants.participants.length; j++) {
                 if (i != j) {
                     var otherId = Participants.participants[j];
                     var otherName = Participants.id_name[otherId];
-                    reputationChoices[userId][otherName] = 0;
+                    this.reputationChoices[userId][otherName] = 0;
                 }
             }
         }
+
+        Neighborhoods.initializeNeighborhoodReputations();
     },
 
     updateReputation: function(userId, targetName, rank) {
         var userName = Participants.id_name[userId];
-        var previousRank = reputationChoices[userName][targetName];
-        reputationChoices[userName][targetName] = rank;
+        var previousRank = this.reputationChoices[userName][targetName];
+        this.reputationChoices[userName][targetName] = rank;
         var diff = rank - previousRank;
-        reputations[targetName] += diff;
+        this.reputations[targetName] += diff;
 
-        Neighborhoods.updateNeighborhoodReputations(reputations);
+        Neighborhoods.updateNeighborhoodReputations(this.reputations);
     },
 }
