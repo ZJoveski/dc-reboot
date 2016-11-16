be1fc41d0b02dd20ff82026056f58557afc7c7f0
huaiyudavid <davidyangrocs@gmail.com>
2016-11-16 02:12:03 -0600

    debugging

--- imports/api/reputations.js
+++ imports/api/reputations.js
@@ -59,5 +59,6 @@ export var Reputations = {
 
         Neighborhoods.updateNeighborhoodReputations(this.reputations);
         Logger.recordReputationChange(userId, targetName, rank);
+        console.log(reputationChoices);
     },
 }
