ff23fb05a8e9c683644c05ad031c056e4c07bb47
huaiyudavid <davidyangrocs@gmail.com>
2016-12-06 07:33:17 -0600

    comment

--- imports/api/experiment_control.js
+++ imports/api/experiment_control.js
@@ -99,6 +99,7 @@ var initializeGame = function() {
 
     /* Log entry. */ Logger.recordSessionInitializationStart(Session.sessionNumber);
 
+    //TODO: currently each adj matrix is different, which causes reputations to glitch
     Session.setAdjMatrix(Parameters.getNextAdjMatrix(proper, Session.sessionNumber));
     /* Log entry. */ Logger.recordNetworkAdjacencyMatrix(Session.adjMatrix);
 
