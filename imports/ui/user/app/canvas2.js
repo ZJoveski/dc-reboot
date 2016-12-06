edd2f07292abaa881a59b0c3016d70e459211193
huaiyudavid <davidyangrocs@gmail.com>
2016-12-06 05:04:07 -0600

    debug

--- imports/ui/user/app/canvas2.js
+++ imports/ui/user/app/canvas2.js
@@ -2,12 +2,12 @@ import { Session } from './../../../api/session.js';
 import './../../../api/meteormethods/game_methods.js';
 
 export var Canvas = function() {
-    const width = 1000;
-    const height = 600;
-
     var vpWidth = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
     var vpHeight = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
     var vpMin = Math.min(vpWidth, vpHeight);
+
+    const width = vpWidth;
+    const height = vpHeight;
 
     var freeWidth = ((vpWidth - 0.37 * vpMin - 0.25 * vpWidth)/vpWidth) * width;
     var freeHeight = height;
