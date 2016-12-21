74bb0a1c92e5710f4811f86636aea11f878e0a56
huaiyudavid <davidyangrocs@gmail.com>
2016-12-20 23:49:41 -0500

    change input data

--- imports/api/parameters.js
+++ imports/api/parameters.js
@@ -52,21 +52,21 @@ export var Parameters = {
         // batchConfigs = readBatchConfig("treatments/test_size_3 (adversarial)/BC_test.txt");
 
         // Full Experiment Data
-        // practiceAdjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix_practice.txt");
-        // practiceParameterValues = readNetConfig("treatments/input_data/network_configuration_practice.txt");
-        // practiceBatchConfigs = readBatchConfig("treatments/input_data/batch_configuration_practice.txt");
+        practiceAdjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix_practice.txt");
+        practiceParameterValues = readNetConfig("treatments/input_data/network_configuration_practice.txt");
+        practiceBatchConfigs = readBatchConfig("treatments/input_data/batch_configuration_practice.txt");
         
-        // adjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix.txt");
-        // parameterValues = readNetConfig("treatments/input_data/network_configuration.txt");
-        // batchConfigs = readBatchConfig("treatments/input_data/batch_configuration.txt");
+        adjacencyMatrices = readAdjMatrix("treatments/input_data/adjacency_matrix.txt");
+        parameterValues = readNetConfig("treatments/input_data/network_configuration.txt");
+        batchConfigs = readBatchConfig("treatments/input_data/batch_configuration.txt");
 
-        practiceAdjacencyMatrices = readAdjMatrix("treatments/test_data (20-25 nodes)/AMP_test.txt");
-        practiceParameterValues = readNetConfig("treatments/test_data (20-25 nodes)/NCP_test.txt");
-        practiceBatchConfigs = readBatchConfig("treatments/test_data (20-25 nodes)/BCP_test.txt");
+        // practiceAdjacencyMatrices = readAdjMatrix("treatments/test_data (20-25 nodes)/AMP_test.txt");
+        // practiceParameterValues = readNetConfig("treatments/test_data (20-25 nodes)/NCP_test.txt");
+        // practiceBatchConfigs = readBatchConfig("treatments/test_data (20-25 nodes)/BCP_test.txt");
         
-        adjacencyMatrices = readAdjMatrix("treatments/test_data (20-25 nodes)/AM_test.txt");
-        parameterValues = readNetConfig("treatments/test_data (20-25 nodes)/NC_test.txt");
-        batchConfigs = readBatchConfig("treatments/test_data (20-25 nodes)/BC_test.txt");
+        // adjacencyMatrices = readAdjMatrix("treatments/test_data (20-25 nodes)/AM_test.txt");
+        // parameterValues = readNetConfig("treatments/test_data (20-25 nodes)/NC_test.txt");
+        // batchConfigs = readBatchConfig("treatments/test_data (20-25 nodes)/BC_test.txt");
 
         // Determine the number of practice and proper games from the size of the arrays loaded above
         this.practiceGames = Math.min(practiceAdjacencyMatrices.length, practiceParameterValues.length);
